986ad7eff8cd173d269638f3923fa603d80e7183
Usman Ahmad <work.usmanahmad@gmail.com>
2013-04-24 11:59:28 +0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SalatQareeb::Application.config.secret_token = 'c5fb636b34e139fee959a9f39a69e51c4febb62fd63ae13fbfd5ff38763fedade3dc4398e65a95f959e6a45c9d878d662276665bbf9e5f15a682659580e3b04d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SalatQareeb::Application.config.session_store :cookie_store, key: '_salat_qareeb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SalatQareeb::Application.config.session_store :active_record_store
